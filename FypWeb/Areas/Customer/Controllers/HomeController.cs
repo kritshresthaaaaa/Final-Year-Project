90dadb438b9345dbce89f434e5fada170b5af4d7
kritshresthaaaa <shresthakrit1010@gmail.com>
2024-03-14 00:01:01 +0545

    recommendation UI

---
+++ FypWeb/Areas/Customer/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FypWeb.Areas.Customer.Controllers
+{
+ 
+    [Area("Customer")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Index","_Customers");
+        }
+    }
+}
